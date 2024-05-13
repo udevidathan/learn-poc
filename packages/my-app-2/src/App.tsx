34923198439e59b6535ffa956fc3750706184ee5
udevidathan <112862965+udevidathan@users.noreply.github.com>
2024-05-13 07:15:37 +0530

    Initial commit

---
+++ packages/my-app-2/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+  <p>my-app-2</p>
+  );
+}
+
+export default App;
---
+++ packages/my-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <p>My-app -1</p>
+  );
+}
+
+export default App;
