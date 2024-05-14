4fc04c9a95b5c965088dc466214e3a4913bb188a
udevidathan <112862965+udevidathan@users.noreply.github.com>
2024-05-14 11:14:32 +0530

    Testing configurations

--- packages/mbo-lib/src/index.tsx
+++ packages/mbo-lib/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import {sum} from "mbo-utils";
+import {sum} from "mbo-utils"
+import "../../styles/index.scss";
 
 export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries , {sum(1,2)}</div>;
+  return <div className='highlight'>the snozzberries taste like snozzberries , {sum(1,2)}</div>;
 };
